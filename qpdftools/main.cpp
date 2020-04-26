41f79fac71a820abcea04cb5d8dfda8cb6b21205
Silas Henrique <silash35@gmail.com>
2020-04-25 22:12:40 -0300

    fixing Debian bugs

---
+++ qpdftools/main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QTranslator t;
+
+    if(QLocale::system().name() == QString("pt_BR")){
+        t.load("/usr/lib/qpdftools/qpdftools_pt_BR.qm");
+        a.installTranslator(&t);
+    }
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
