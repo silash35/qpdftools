3edbe2e93427709eec811d8b71928787517390b3
Silas Henrique <silash35@gmail.com>
2025-03-08 21:43:41 -0300

    Increase wait time to process

--- src/api/externalSoftware.cpp
+++ src/api/externalSoftware.cpp
@@ -9,25 +9,24 @@ void ExternalSoftware::run(const QStringList &arguments, const QString &dir) {
   // Set working directory
   if (dir != "default") {
     process.setWorkingDirectory(dir);
-    qInfo() << "Using working directory: " << dir << "\n";
+    qInfo() << "Using working directory: " << dir;
   }
 
   // Start process
-  qInfo() << "Executing " << softwareName << " with the arguments:" << arguments << "\n";
+  qInfo() << "Executing " << softwareName << " with the arguments:" << arguments;
   process.start(softwareCommand, arguments, QIODevice::ReadWrite);
 
   process.closeWriteChannel();
 
   // Wait for process to finish
-  process.waitForFinished();
-  qInfo() << "finished to execute " + softwareName << "\n";
+  process.waitForFinished(120 * 1000); // Increase wait time to 2 minutes, for large files
 
   // Check for errors
   QString error = process.readAllStandardError();
   if (!error.isEmpty()) {
-    qCritical() << "Error in " + softwareName + ": " + error << "\n";
+    qCritical() << "Error in " + softwareName + ": " + error;
     throw "Error in " + softwareName + ": " + error;
   }
 
-  qInfo() << "Finished executing " + softwareName;
+  qInfo() << "Finished executing " + softwareName << "\n";
 }
